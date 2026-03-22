1fce27fdc872314f30749aafc6b25ecf09286450
MrAlexOm <alexom@example.com>
2026-03-23 03:26:24 +0400

    Test StyleSheet in HomeScreen

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 const HomeScreen = () => {
   console.log("HomeScreen is rendering");
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#FF0000' }}>
-      <Text style={{ color: 'white', fontSize: 24 }}>HOME SCREEN WORKS!</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>HOME SCREEN WORKS!</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FF0000',
+  },
+  title: {
+    color: 'white',
+    fontSize: 24,
+  },
+});
 
 export default HomeScreen;
