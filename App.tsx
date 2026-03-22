62eb130e705360d2f22ac254b122cb53679b2e6c
MrAlexOm <alexom@example.com>
2026-03-23 02:51:38 +0400

    Test NavigationContainer with TEST OK

--- App.tsx
+++ App.tsx
@@ -78,7 +78,9 @@ export default function App() {
   
   return (
     <NavigationContainer>
-      <AppNavigator />
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#0B0B0B' }}>
+        <Text style={{ color: 'white', fontSize: 24 }}>TEST OK</Text>
+      </View>
     </NavigationContainer>
   );
 }
